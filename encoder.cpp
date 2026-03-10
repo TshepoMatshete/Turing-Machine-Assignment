da231e81af3202c13cef37b429e8739de7537fd0
TshepoMatshete <2627330@students.wits.ac.za>
2026-03-10 20:44:05 +0200

    just did question 1... where i worked on the encoder.

---
+++ encoder.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<char, string> parseLibrary(const string& lib) {
+    unordered_map<char, string> encoding;
+    stringstream ss(lib);
+    string pair;
+
+    while (getline(ss, pair, '|')) {
+        char key = pair[0];                
+        string value = pair.substr(2);    
+        encoding[key] = value;
+    }
+
+    return encoding;
+}
+
+string encode(const string& lib, const string& text) {
+    unordered_map<char, string> encoding = parseLibrary(lib);
+    string result = "";
+
+    for (char c : text) {
+        if (encoding.count(c)) {
+            result += encoding[c];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    string library,text;
+
+    cin>> library;
+    cin>> text;
+
+    string encoded = encode(library, text);
+
+    cout << encoded << endl;
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string encoding;
+    cin >> encoding;
+ 
+    cout<< "the string read is: " << encoding << endl;
+
+    return 0;
+}
